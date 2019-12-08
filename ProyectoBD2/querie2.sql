7feebf2dce5300b82ae791840365472e97d23c1a
benitez4721 <32308086+benitez4721@users.noreply.github.com>
2019-12-07 23:07:31 -0400

    Add files via upload

--- ProyectoBD2/querie2.sql
+++ ProyectoBD2/querie2.sql
@@ -1,7 +1,7 @@
 --¿Cuántas subastas se realizan por mes por categoría? (tomar en cuenta todas las categorías) 
 
-SELECT EXTRACT(MONTH FROM SUBASTA.fecha_init) as mes,categoria.nombre_cat,count(*) as Nro_Subastas  FROM SUBASTA 
+SELECT EXTRACT(YEAR FROM SUBASTA.fecha_init)as  year_, EXTRACT(MONTH FROM SUBASTA.fecha_init) as month_,categoria.nombre_cat,count(*) as Nro_Subastas  FROM SUBASTA 
 INNER JOIN PERTENECE  ON PERTENECE.id_prod = SUBASTA.id_prod
 INNER JOIN CATEGORIA ON PERTENECE.id_cat = CATEGORIA.id_cat
-GROUP by mes, categoria.nombre_cat
-ORDER BY mes
+GROUP by month_,year_, categoria.nombre_cat
+ORDER BY year_,month_
